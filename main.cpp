ca5a02194df496db17d05f5d646093e11486fc80
edsonadn <edsonarturodimasn@hotmail.com>
2020-11-20 15:50:21 -0600

    Se a aislado completamente la funcion dibujo

--- main.cpp
+++ main.cpp
@@ -1,24 +1,18 @@
 #include <stdio.h>
 #include <windows.h>
 
-void dibujo(int x,int y,HANDLE dv){
-    COORD dwPos;
-    dwPos.X=x;
-    dwPos.Y=y;
-    SetConsoleCursorPosition(dv,dwPos);
-    printf("#");
-}
-
-int main(){
+void dibujo(int x,int y){
     HANDLE hCon;
     COORD dwPos;
     hCon = GetStdHandle(STD_OUTPUT_HANDLE);
-    dibujo(4,2,hCon);
-    dibujo(6,2,hCon);
-    dibujo(3,4,hCon);
-    dibujo(7,4,hCon);
-    dibujo(4,4,hCon);
-    dibujo(5,4,hCon);
-    dibujo(6,4,hCon);
+    dwPos.X=x;
+    dwPos.Y=y;
+    SetConsoleCursorPosition(hCon,dwPos);
+    printf("*");
+}
+
+int main(){
+    dibujo(4,2);
+    return 0;
 }
 
